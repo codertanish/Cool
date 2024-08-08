03788fc560069c91c947b92b256046daf7934132
codertanishrastogi <tanishrastogi2020@gmail.com>
2024-08-08 19:45:30 -0400

    Create Action.java and Person.java

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person {
+    
+    int age = 16;
+    public void older() {
+        age++;
+    }
+
+    public void younger() {
+        age--;
+    }
+
+    public void print(){
+        System.out.println(age);
+    }
+    
+}
+
+
---
+++ action.java
@@ -0,0 +1,8 @@
+public class action {
+    public static void main(String[] args) {
+        Person bob = new Person();
+        bob.older();
+        bob.print();
+    }
+
+}
