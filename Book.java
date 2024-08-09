a21635f10b0a1c9883bfea79b9f2ca83793308ff
codertanishrastogi <tanishrastogi2020@gmail.com>
2024-08-09 15:29:54 -0400

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,35 @@
+public class Book {
+public static class BookDefinition {
+    String name = "Book";
+    String author = "Person";
+    double price = 10;
+    boolean soldStatus = false;
+    
+    public void sell() {
+        soldStatus = true;
+    }
+
+    public void rename(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        if (soldStatus == true) {
+            return "Book has been sold";
+        }
+        return "Book has not been sold";
+    }
+}
+
+public static void main(String[] args) {
+    BookDefinition myBook = new BookDefinition();
+    System.out.println("Book Name: " + myBook.name);
+    myBook.getStatus();
+    myBook.sell();
+    myBook.rename("myBook");
+    System.out.println("Book Name: " + myBook.name);
+    myBook.getStatus();
+
+    }
+}
+
