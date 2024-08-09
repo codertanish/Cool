ce16a949b2ffb5e730f7aebf94a6ec5b52bf2530
codertanishrastogi <tanishrastogi2020@gmail.com>
2024-08-09 19:26:56 -0400

    Updated code.

--- Lasagna.java
+++ Lasagna.java
@@ -1,5 +1,5 @@
 public class Lasagna {
-    final double LASAGNA_MINUTES_IN_OVEN = 40;
+    private final double LASAGNA_MINUTES_IN_OVEN = 40;
 public double expectedMinutesInOven() {
     return LASAGNA_MINUTES_IN_OVEN;
 }
--- Robot.java
+++ Robot.java
@@ -1,5 +1,5 @@
 public class Robot {
-double motorSpeed = 0.0;
+private double motorSpeed = 0.0;
 final double METERS_PER_FIVE_ROTATIONS = 1; //the number of meters added to a torus launch per every five rotations added to RPM.
 double rpm = 0.0;
 //analyze distance using AprilTags/Greenlight let x be horizontal distance and y be vertical distance; let the center of the field be (0,0).
