6b76767879203886efa174b1cafaf4006e9f46f7
codertanishrastogi <tanishrastogi2020@gmail.com>
2024-08-10 19:04:05 -0400

    Update SensorRobot.java

--- SensorRobot.java
+++ SensorRobot.java
@@ -3,8 +3,12 @@ public class SensorRobot {
     RobotAction drive = new RobotAction();
 
 
-public static void main(String[] args) {
-    SensorRobot myRobot = new SensorRobot();
+public static void main(String[] args) { //I could remove the static from here, would this work?
+    SensorRobot myRobot = new SensorRobot(); //I ended up just using the full class as an object.
+    /*I also could have just added the following code here rather than outside of the main loop:
+    Sensor distanceSensor = new Sensor(5, 10);
+    RobotAction drive = new RobotAction();
+    */
     
     System.out.println("Threshold Reached: " + myRobot.distanceSensor.checkSensor());
     myRobot.distanceSensor.setCurrentValue(10);
